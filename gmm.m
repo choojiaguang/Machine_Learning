dfa615e5e08678ad48d1083e28fc2ab210bf089a
Brian-Azizi <brian.m.azizi@gmail.com>
2015-12-09 11:00:31 +0000

    Plot looks a little better now

--- gmm.m
+++ gmm.m
@@ -7,20 +7,19 @@ rngsetting = rng;  % save the current rng settings for reproducability
 % Generate Gaussian clusters of data
 
 % True number of clusters:
-K = 12;
+K = 30;
 
 % Number of clusters in GMM algorithm
-C = 6;
+C = 2;
  
 MU = zeros(K, 2);
 SIGMA = zeros(2, 2, K);
 X = zeros(K * 1000, 2);
 for k = 1:K;
-    MU(k,:) = 2*K*rand(1, 2);
+    MU(k,:) = 2*K*(rand(1, 2)-0.5);
     s = (rand(2,2) - 0.5);
     SIGMA(:,:,k) = 0.5*K*(s'*s + eye(2));
     %SIGMA(:,:,k) = eye(2);
-    disp(k);
     X(1000*(k-1)+1 : 1000*k , :) = mvnrnd(MU(k,:), SIGMA(:,:,k), 1000);
 end
 
@@ -30,9 +29,16 @@ hold on
 set(gca, 'color', [0 0 0])
 
 % Fit a two-component GMM
-options = statset('Display', 'final','MaxIter',50*C);  % display information for the algorithm
+options = statset('Display', 'iter','MaxIter',50*C);  % display information for the algorithm
 obj = fitgmdist(X, C, 'Options', options);
 
 % Plot the fit
 h = ezcontour(@(x,y)pdf(obj, [x y]), ...
     [min(X(:,1)) - 1, max(X(:,1))+1], [min(X(:,2))-1, max(X(:,2))+1]);
+centroids = obj.mu;
+plot(centroids(:,1),centroids(:,2),'g*','MarkerSize',12);
+legend('fitted centroid');
+xlabel('Feature 1','FontSize',16);
+ylabel('Feature 2','FontSize',16);
+title(['Gaussian Mixture Model with K = ',num2str(C),...
+    '  (generated using ', num2str(K),' MVGs)'],'FontSize',20);
